6bca33638572794286fcaf6d34b8df5db8d03e54
Vitalii Samofal <vitalii.samofal@softkit.dev>
2024-05-15 13:13:15 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbarProvider } from "@refinedev/kbar";
+
+import {
+  ErrorComponent,
+  ThemedLayoutV2,
+  useNotificationProvider,
+} from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import routerBindings, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerBindings}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  useNewQueryKeys: true,
+                  projectId: "AeIZg8-YgnWrd-mUXPwC",
+                }}
+              >
+                <Routes>
+                  <Route
+                    element={
+                      <ThemedLayoutV2
+                        Sider={(props) => null}
+                      >
+                        <Outlet />
+                      </ThemedLayoutV2>
+                    }
+                  >
+                    <Route path="*" element={<ErrorComponent />} />
+                  </Route>
+                </Routes>
+
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
